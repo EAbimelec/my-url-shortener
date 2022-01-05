b57035a81c976d724a5686829de2dc542ddfa06d
EAbimelec <abimelec70.arenas@gmail.com>
2022-01-04 23:15:57 -0600

    Add logout feature
    Using the cookies the app can recognize if the user is logged or not.

--- express_server.js
+++ express_server.js
@@ -3,12 +3,14 @@ const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const generateShortURL = require('./randomString');
+const cookieParser = require("cookie-parser");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -21,17 +23,28 @@ app.listen(PORT, () => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]  
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"]
+  };
+  
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const sURL = req.params.shortURL;
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[sURL]};
+  const templateVars = { 
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[sURL]
+  };
+
   res.render("urls_show", templateVars);
 });
 
@@ -70,8 +83,14 @@ app.post("/urls/:shortURL", (req, res) => {
 app.post("/login", (req, res) => {
 
   const username = req.body.username;
-  console.log(username);
+
   res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
